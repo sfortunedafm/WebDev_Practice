0907ce842ac907ae0ef9d36dd721b951f32851b3
stephen fortune <22stephenfortune@libertiescollege.ie>
2023-05-05 10:03:13 +0100

    minimum viable working, routes established and material added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {BlogComponent} from "./blog/blog.component";
+import {StoreComponent} from "./store/store.component";
+import {CheckoutComponent} from "./checkout/checkout.component";
+import {SubscribeComponent} from "./subscribe/subscribe.component";
+
+const routes: Routes =[
+  {path: 'blog', component: BlogComponent},
+  {path: 'store', component: StoreComponent},
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'subscribe', component: SubscribeComponent}
+]
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { StoreComponent } from './store/store.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { AppRoutingModule } from './app-routing.module';
 
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatGridListModule} from "@angular/material/grid-list";
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    StoreComponent,
+    SubscribeComponent,
+    CheckoutComponent,
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
