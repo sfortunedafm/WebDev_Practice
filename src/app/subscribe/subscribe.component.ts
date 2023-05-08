03e3980a6166f18130cf0150607c3d90b8bc2b0e
stephen fortune <22stephenfortune@libertiescollege.ie>
2023-05-08 14:44:00 +0100

    cleaning out old comments addition of boilerplate to template

--- src/app/subscribe/subscribe.component.ts
+++ src/app/subscribe/subscribe.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-// import {FormControl, FormGroup, Validators} from "@angular/forms";
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 export interface Tile {
   color: string;
   cols: number;
@@ -24,38 +23,5 @@ export class SubscribeComponent {
     console.log("button pressed");
     console.log(this.registerForm.value);
   }
-
-  // // form = new FormGroup({
-  // //   emailFormControl : new FormControl('', [Validators.required, Validators.email]),
-  // //   passwordControl : new FormControl('', [Validators.required,
-  // //     Validators.minLength(8)])
-  // // });
-  // form = new FormGroup({
-  //   first: new FormControl('Nancy', Validators.minLength(2)),
-  //   last: new FormControl('Drew'),
-  //   Email: new FormControl('', [Validators.required, Validators.email]),
-  //   Password: new FormControl('', [Validators.required,
-  //     Validators.minLength(8)])
-  // });
-  // onSubmit(): void {
-  //   console.log(this.form.value);  // {first: 'Nancy', last: 'Drew'}
-  // }
-  // get first(): any {
-  //   return this.form.get('first');
-  // }
-  // // setValue() {
-  // //   this.form.setValue({first: 'Carson', last: 'Drew'});
-  // // }
-  // emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  // passwordControl = new FormControl('', [Validators.required,
-  //   Validators.minLength(8)]);
-  // // tiles: Tile[] = [
-  // //   {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-  // //   {text: 'Sidebar Space', cols: 1, rows: 2, color: 'lightgreen'},
-  // //   {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-  // //   {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  // // ];
-  hide = true;
-
-
+  // hide = true; for use with confirm password field
 }
